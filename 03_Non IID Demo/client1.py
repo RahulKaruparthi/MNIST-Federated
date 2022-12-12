3284b2daa2e346ef24fefc4198681f560a17ae66
RahulKaruparthi <108293302+RahulKaruparthi@users.noreply.github.com>
2022-12-12 16:42:24 +0530

    Update client1.py

--- 03_Non IID Demo/client1.py
+++ 03_Non IID Demo/client1.py
@@ -10,20 +10,13 @@ import seaborn as sns
 import tensorflow as tf
 from tensorflow import keras
 
-# AUxillary methods
-
-
-# <<<<<<< HEAD
-# dirname = os.path.dirname("/mnt/d/fincrime-federated/")
-# =======
-dirname = os.path.dirname('/mnt/d/fincrime-federated/')
-# >>>>>>> 195876fb47a93943c29f0947d5cf614efafacf40
-os.chdir(dirname)
-print(os.getcwd())
 
 import submission_src.fincrime.solution_centralized as funcs
-
 importlib.reload(funcs)
+
+dirname = os.path.dirname('/mnt/d/fincrime-federated/')
+os.chdir(dirname)
+
 
 start_time = time.time()
 
@@ -39,51 +32,49 @@ datapathjsonString = "data/fincrime/centralized/train/trail_data_1.json"
 swift_data_path = funcs.json_to_dict(datapathjsonString)["swift_data_path"]
 bank_data_path = funcs.json_to_dict(datapathjsonString)["bank_data_path"]
 
+X_train,Y_train = funcs.fit(swift_data_path = swift_data_path,
+                    bank_data_path = bank_data_path,
+                    model_dir = model_dir,
+                    preds_format_path = preds_format_path,
+                    preds_dest_path = preds_dest_path,
+                    m = 'xgboost')
+
+
 # predict on test data
+datapathjsonString = 'data/fincrime/centralized/test/data.json'
+swift_data_path = funcs.json_to_dict(datapathjsonString)['swift_data_path']
+bank_data_path = funcs.json_to_dict(datapathjsonString)['bank_data_path']
+
+X_test,Y_test = funcs.predict(
+                    swift_data_path =swift_data_path,
+                    bank_data_path = bank_data_path,
+                    model_dir = model_dir,
+                    preds_format_path = preds_format_path,
+                    preds_dest_path = preds_dest_path,
+                    m = 'xgboost'
+                    )   
 
 # Define Flower client
 class FlowerClient(fl.client.NumPyClient):
-    def get_parameters(self, config):
+    def get_parameters(self,config):
         return model.get_weights()
 
-    #     def fit(self, parameters, config):
-    #         model.set_weights(parameters)
-    #         r = model.fit(x_train, y_train, epochs=1, validation_data=(x_test, y_test), verbose=0)
-    #         hist = r.history
-    #         print("Fit history : " ,hist)
-    #         return model.get_weights(), len(x_train), {}
-    def fit(self):  # , parameters, config):
-        result = funcs.fit(
-            swift_data_path=swift_data_path,
-            bank_data_path=bank_data_path,
-            model_dir=model_dir,
-            preds_format_path=preds_format_path,
-            preds_dest_path=preds_dest_path,
-            m="xgboost",
-        )
-        return result
+    def fit(self, parameters, config):
+        model.set_weights(parameters)
+        r = model.fit(x_train, y_train, epochs=1, validation_data=(x_test, y_test), verbose=0)
+        hist = r.history
+        print("Fit history : " ,hist)
+        return model.get_weights(), len(x_train), {}
 
-    #     def evaluate(self, parameters, config):
-    def evaluate(self):  # , parameters, config):
-        #         model.set_weights(parameters)
-        #         loss, accuracy = model.evaluate(x_test, y_test, verbose=0)
-        #         print("Eval accuracy : ", accuracy)
-        result = funcs.predict(
-            swift_data_path=swift_data_path,
-            bank_data_path=bank_data_path,
-            model_dir=model_dir,
-            preds_format_path=preds_format_path,
-            preds_dest_path=preds_dest_path,
-            m="xgboost",
-        )
-        # return loss, len(x_test), {"accuracy": accuracy}
-        return result
+    def evaluate(self, parameters, config):
+        model.set_weights(parameters)
+        loss, accuracy = model.evaluate(x_test, y_test, verbose=0)
+        print("Eval accuracy : ", accuracy)
+        return loss, len(x_test), {"accuracy": accuracy}
 
-    # Start Flower client
-
-
+# Start Flower client
 fl.client.start_numpy_client(
-    server_address="localhost:" + str(sys.argv[1]),
-    client=FlowerClient(),
-    grpc_max_message_length=1024 * 1024 * 1024,
+        server_address="localhost:"+str(sys.argv[1]), 
+        client=FlowerClient(), 
+        grpc_max_message_length = 1024*1024*1024
 )
